04b49703ff1e577f5890a43c53ad9d2930408460
Mitch <mitch@tabian.ca>
2018-04-30 18:24:35 -0700

    Module_4.4_Start

--- app/src/main/java/courses/pluralsight/com/tabianconsulting/issues/IssuesFragment.java
+++ app/src/main/java/courses/pluralsight/com/tabianconsulting/issues/IssuesFragment.java
@@ -7,36 +7,78 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import courses.pluralsight.com.tabianconsulting.R;
+import courses.pluralsight.com.tabianconsulting.utility.ResultCodes;
 
 
 /**
  * Created by User on 4/16/2018.
  */
 
-public class IssuesFragment extends Fragment  {
+public class IssuesFragment extends Fragment implements
+        View.OnClickListener
+{
 
     private static final String TAG = "IssuesFragment";
 
     //widgets
+    private ImageView mAddIcon;
 
 
     //vars
+    private IIssues mIIssues;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_issues, container, false);
+        mAddIcon = view.findViewById(R.id.add_new);
 
+        mAddIcon.setOnClickListener(this);
 
         return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.add_new:{
+                //go to NewIssueActivity
+                Intent intent = new Intent(getActivity(), NewIssueActivity.class);
+                startActivityForResult(intent, ResultCodes.SNACKBAR_RESULT_CODE);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            mIIssues = (IIssues) getActivity();
+        }catch (ClassCastException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode == ResultCodes.SNACKBAR_RESULT_CODE){
+            Log.d(TAG, "onActivityResult: building snackbar message.");
+            String message = data.getStringExtra(getString(R.string.intent_snackbar_message));
+            mIIssues.buildSnackbar(message);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
 
 
